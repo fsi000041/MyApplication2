c9afea54880426a36dcd9270f6351d613559ae92
fsi000044@163.com <52030452A>
2019-04-07 10:30:35 +0800

    新电脑 --测试提交

--- app/src/main/java/com/example/caoyang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/caoyang/myapplication/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         setContentView(R.layout.activity_main);
         Button startBtn = findViewById(R.id.button);
         SwipeMenuView swipeMenuView =  findViewById(R.id.swipeMenuView);
